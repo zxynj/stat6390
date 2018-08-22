2cf0533e8afde7ea293c57ebbab797970a4802cf
Steven Chiou <schiou@utdallas.edu>
2018-08-22 18:43:55 -0500

    adding note3

--- notes/pdfKnitr.sh
+++ notes/pdfKnitr.sh
@@ -3,13 +3,13 @@
 rm .RData
 rm .Rhistory
 
-echo 'knitr::knit("note2.Rnw")' > toKnitr.R
+echo 'knitr::knit("note3.Rnw")' > toKnitr.R
 
 R CMD BATCH toKnitr.R
-pdflatex note2.tex
-bibtex note2.aux
-pdflatex note2.tex
-pdflatex note2.tex
+pdflatex note3.tex
+bibtex note3.aux
+pdflatex note3.tex
+pdflatex note3.tex
 
 rm *log
 rm *out
@@ -25,5 +25,6 @@ rm *snm
 rm *toc
 rm *nav
 rm *vrb
+rm *~
 rm .RData
 rm toKnitr.R
